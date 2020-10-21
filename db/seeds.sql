23b6de221659144692e35209612cf060cdc3cd9d
malex19889 <malex19889@yahoo.com>
2020-10-21 16:07:40 -0500

    adding db schema and seed data, and basic server

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name)
+    VALUES ('bacon burger');
+    
+INSERT INTO burgers (burger_name)
+    VALUES ('mushroom swiss burger');
+    
+INSERT INTO burgers (burger_name)
+    VALUES ('avocado burger');
